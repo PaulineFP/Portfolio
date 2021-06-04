84aa240b28fab2e041420fc88a2638e1ef9ed995
Pauline <pauline.frare-pradal@laposte.net>
2021-06-04 15:29:31 +0200

    ci: update mail address

--- contact.php
+++ contact.php
@@ -14,10 +14,10 @@ Elle utilise le même moteur de recherche que fgetss().
 
 
 //Envoi d'e-mail et réception des informations:
-$to = 'p.frare@codeur.online';
+$to = 'pauline.frare-pradal@laposte.net';
 $email_subject = "Contact: $name";
 $email_body = "Vous avez reçu un nouveau message depuis le formulaire de contact de votre portfolio.\n\n"."Voici les détails:\n\nName: $name\n\nEmail:$email_add\n\nObjet:$object\n\nMessage:$message";
-$headers = "From: noreply-p.frare@codeur.online\n";
+$headers = "From: noreply-pauline.frare-pradal@laposte.net\n";
 $headers = "Répondre à : $email_add";
 mail($to,$email_subject,$email_body,$headers);
 return true;
